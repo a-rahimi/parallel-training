ed38c3eab8855be87af20b2ecabdae6a2c67fe21
Ali Rahimi <ali.rahimi@getcruise.com>
2025-01-15 22:35:05 -0800

    cuckoo parallelism

--- parallelsim.py
+++ parallelsim.py
@@ -1,6 +1,7 @@
 from typing import Callable, Dict, List, NamedTuple, Set, Tuple
 
 import collections
+import dataclasses as dc
 from enum import Enum
 import itertools
 
@@ -32,7 +33,7 @@ class Work(NamedTuple):
     def duration(self) -> int:
         return 1 if self.direction == Direction.FORWARD else 2
 
-    def next(self, num_stages: int) -> "Work":
+    def successor(self, num_stages: int) -> "Work":
         if self.is_final():
             return self
         elif self.direction == Direction.FORWARD and self.stage == num_stages - 1:
@@ -54,32 +55,40 @@ class ComputeAndWeightWorkers(NamedTuple):
     weight_worker: Worker
 
 
+@dc.dataclass(frozen=True)
 class PriorityOrder:
-    def __init__(self, num_stages: int, num_batches: int):
-        self.order: Dict[Work, int] = {}
+    order: Dict[Work, int]
 
     def __call__(self, work: Work) -> int:
         return self.order[work]
 
 
-class BackwardFirst(PriorityOrder):
+class OldestStageFirst(PriorityOrder):
     def __init__(self, num_stages: int, num_batches: int):
-        self.order = {
-            Work(stage, batch, direction): n
-            for n, (stage, batch, direction) in enumerate(
-                itertools.product(range(-1, num_stages), range(num_batches), Direction)
-            )
-        }
+        super().__init__(
+            {
+                Work(stage, batch, direction): n
+                for n, (stage, direction, batch) in enumerate(
+                    itertools.product(
+                        range(-1, num_stages), Direction, range(num_batches)
+                    )
+                )
+            }
+        )
 
 
 class OldestBatchFirst(PriorityOrder):
     def __init__(self, num_stages: int, num_batches: int):
-        self.order = {
-            Work(stage, batch, direction): n
-            for n, (batch, direction, stage) in enumerate(
-                itertools.product(range(num_batches), Direction, range(-1, num_stages))
-            )
-        }
+        super().__init__(
+            {
+                Work(stage, batch, direction): n
+                for n, (batch, direction, stage) in enumerate(
+                    itertools.product(
+                        range(num_batches), Direction, range(-1, num_stages)
+                    )
+                )
+            }
+        )
 
 
 class WorkHistory(NamedTuple):
@@ -89,23 +98,18 @@ class WorkHistory(NamedTuple):
     performed_by: Worker
 
 
+@dc.dataclass
 class SimulationStats:
-    def __init__(
-        self,
-        num_workers: int,
-        num_stages: int,
-        num_batches: int,
-        schedule: Callable[[Stage, Batch], ComputeAndWeightWorkers],
-    ) -> None:
-        self.num_workers = num_workers
-        self.num_stages = num_stages
-        self.num_batches = num_batches
-        self.schedule = schedule
+    num_workers: int
+    num_stages: int
+    num_batches: int
+    schedule: Callable[[Work], ComputeAndWeightWorkers]
 
+    def __post_init__(self) -> None:
         self.all_work: List[WorkHistory] = []
         self.df_worker_stats = pd.DataFrame(
             0,
-            index=range(num_workers),
+            index=range(self.num_workers),
             columns=[
                 "Weight transmissions",
                 "Weight storage",
@@ -121,65 +125,85 @@ class SimulationStats:
             set
         )
 
-    def add(
+    def update(
         self,
-        pred_work: Work,
+        frontier_work: Work,
         new_work: Work,
         start_time: Timestamp,
         end_time: Timestamp,
     ) -> None:
-        pred_worker = self.schedule(pred_work).compute_worker
+        frontier_worker = self.schedule(frontier_work).compute_worker
         new_worker, weight_worker = self.schedule(new_work)
 
         # Record the work.
         self.all_work.append(WorkHistory(new_work, start_time, end_time, new_worker))
 
-        # Update transmission statistics.
-        stat_row = self.df_worker_stats.loc[new_worker]
-        stat_row["Weight transmissions"] += new_worker != weight_worker
-        stat_row["Activation transmissions"] += new_worker != pred_worker
-        stat_row["Activation storage"] += new_work.direction.value
-        stat_row["Max activation storage"] = max(
-            stat_row["Max activation storage"], stat_row["Activation storage"]
+        worker_stats = self.df_worker_stats.loc[new_worker]
+
+        # This work doesn't own its weights. Grab them from the owner.
+        worker_stats["Weight transmissions"] += new_worker != weight_worker
+
+        # Grab the activations from the predecessor.
+        worker_stats["Activation transmissions"] += new_worker != frontier_worker
+
+        if new_work.direction == Direction.FORWARD:
+            backward_worker = self.schedule(
+                Work(new_work.stage, new_work.batch, Direction.BACKWARD)
+            ).compute_worker
+            if new_worker == backward_worker:
+                # This worker stores its input until it computes the backward
+                # pass.
+                worker_stats["Activation storage"] += 1
+            else:
+                # This worker transmits its input to backward_worker, which stores
+                # it until it computes the backward pass.
+                worker_stats["Activation transmissions"] += 1
+                self.df_worker_stats.loc[backward_worker, "Activation storage"] += 1
+        else:
+            # As soon as the backward pass finishes, the worker can free its
+            # input.
+            worker_stats["Activation storage"] -= 1
+
+        worker_stats["Max activation storage"] = max(
+            worker_stats["Max activation storage"], worker_stats["Activation storage"]
         )
 
+        # Collect weights owned by this worker.
         if new_worker == weight_worker:
             self.worker_weight_storage[new_worker].add(new_work.stage)
 
-    def max_time(self) -> Timestamp:
+    def end_time(self) -> Timestamp:
         return max(work_history.end_time for work_history in self.all_work)
 
-    def render_work_produced(self) -> str:
+    def render_work_produced(self) -> display.HTML:
         # Populate a 2D array that represents the pipeline diagram.
         pipeline_diagram = np.full(
-            (self.num_workers, self.max_time()), "<td colspan=1></td>", object
+            (self.num_workers, self.end_time()), "<td colspan=1></td>", object
         )
-        for work_history in self.all_work:
-            length = work_history.end_time - work_history.start_time
-            pipeline_diagram[work_history.performed_by, work_history.start_time] = (
-                f"<td class='batch{work_history.work.batch}' colspan='{length}'>{work_history.work}</td>"
+        for wh in self.all_work:
+            length = wh.end_time - wh.start_time
+            pipeline_diagram[wh.performed_by, wh.start_time] = (
+                f"<td class='batch{wh.work.batch}' colspan='{length}'>{wh.work}</td>"
             )
-            pipeline_diagram[
-                work_history.performed_by,
-                work_history.start_time + 1 : work_history.end_time,
-            ] = ""
+            pipeline_diagram[wh.performed_by, wh.start_time + 1 : wh.end_time] = ""
 
         # Render the pipeline diagram as an HTML table.
-        max_char_width = 2 + max(
-            len(str(work_history.work)) for work_history in self.all_work
-        )
-        num_batches = 1 + max(work_history.work.batch for work_history in self.all_work)
-        batch_styles = ""
-        for batch in range(num_batches):
-            color = np.array(matplotlib.cm.Set1(batch / num_batches)[:-1])
-            batch_styles += f"""
+        max_char_width = 2 + max(len(str(wh.work)) for wh in self.all_work)
+        batch_styles = "\n".join(
+            f"""
             .timing .batch{batch} {{
                 background-color: {matplotlib.colors.to_hex(color)};
                 border: 1px solid {matplotlib.colors.to_hex(color * 0.9)};
             }}
             """
-        time_header = "".join(f"<th>{t + 1}</th>" for t in range(self.max_time()))
-        content = "\n".join(
+            for batch, color in enumerate(
+                matplotlib.cm.tab20(np.linspace(0, 1, self.num_batches))[:, :-1]
+            )
+        )
+        timing_table_header = "".join(
+            f"<th>{t + 1}</th>" for t in range(self.end_time())
+        )
+        timing_table_rows = "\n".join(
             f"<tr><td>w{worker}</td>" + "".join(pipeline_diagram_row) + "</tr>"
             for worker, pipeline_diagram_row in enumerate(pipeline_diagram)
         )
@@ -218,8 +242,8 @@ class SimulationStats:
             <body>
             <table class='timing' align='center'>
                 <tr><th colspan=100% class='schedule'>{self.schedule.__name__}</th></tr>
-                <tr><th></th>{time_header}</tr>
-                {content}
+                <tr><th></th>{timing_table_header}</tr>
+                {timing_table_rows}
             </table>
             </body>
         """)
@@ -248,22 +272,22 @@ class SimulationStats:
 
     def aggregate_stats(self) -> pd.DataFrame:
         df_worker_stats = self.worker_stats()
-        df = pd.DataFrame(
+        return pd.DataFrame(
             pd.Series(
                 {
-                    "Throughput per worker (jobs / time / worker)": self.num_stages
-                    * self.num_batches
-                    * len(Direction)
-                    / self.max_time()
+                    "Worker throughput (jobs / time / worker)": len(self.all_work)
+                    / self.end_time()
                     / self.num_workers,
                     "Max activation storage for a worker": df_worker_stats[
                         "Max activation storage"
                     ].max(),
-                }
+                    "Mean activation storage for workers": df_worker_stats[
+                        "Max activation storage"
+                    ].mean(),
+                },
+                name="Aggegrate Metrics",
             )
         )
-        df.columns = ["Aggegrate Metrics"]
-        return df
 
     def _repr_html_(self):
         return (
@@ -279,16 +303,18 @@ def simulate(
     num_workers: int,
     num_stages: int,
     num_batches: int,
-    schedule: Callable[[Stage, Batch], ComputeAndWeightWorkers],
-    priority_order: Callable[[Stage, Batch, Direction], int] = None,
+    schedule: Callable[[Work], ComputeAndWeightWorkers],
+    priority_order: Callable[[Work], int] = None,
 ) -> SimulationStats:
     # Simulate a set of workers that greedily grab the next job from a DAG of
-    # jobs. A pleasant aspect of this simulation is that it doesn't maintain a
-    # global clock. Instead, the clock is distributed across various data
-    # structures.
+    # jobs. A pleasant aspect of this simulation is that it's tickless, and it
+    # doesn't doesn't maintain a global clock. Instead of monotonically
+    # advancing a clock at each round of simulation, it finds the work that can
+    # be executed the earliest, and deduces time from it. This simulates the
+    # asynchronous behavior of the workers more closely than a tick-based
+    # simulator.
 
-    if priority_order is None:
-        priority_order = BackwardFirst(num_stages, num_batches)
+    priority_order = priority_order or OldestBatchFirst(num_stages, num_batches)
     stats = SimulationStats(num_workers, num_stages, num_batches, schedule)
 
     # The execution frontier is the set of work that has already executed,
@@ -307,16 +333,18 @@ def simulate(
     while execution_frontier:
         # Find work that can execute the earliest.
         r: List[Tuple[Timestamp, Work, Work, Worker]] = []
-        for work in execution_frontier:
-            new_work = work.next(num_stages)
+        for frontier_work in execution_frontier:
+            new_work = frontier_work.successor(num_stages)
             worker = schedule(new_work).compute_worker
-            when_work_can_start = max(worker_busy_until[worker], work_end_time[work])
-            priority = priority_order(work)
-            r.append((when_work_can_start, priority, work, new_work, worker))
-        when_work_can_start, priority, old_work, new_work, new_worker = min(r)
+            when_work_can_start = max(
+                worker_busy_until[worker], work_end_time[frontier_work]
+            )
+            priority = priority_order(frontier_work)
+            r.append((when_work_can_start, priority, frontier_work, new_work, worker))
+        when_work_can_start, priority, frontier_work, new_work, new_worker = min(r)
 
         # Replace the work work with its descendent.
-        execution_frontier.remove(old_work)
+        execution_frontier.remove(frontier_work)
         execution_frontier.add(new_work)
 
         work_start_time[new_work] = when_work_can_start
@@ -329,8 +357,8 @@ def simulate(
             work for work in execution_frontier if not work.is_final()
         }
 
-        stats.add(
-            old_work, new_work, work_start_time[new_work], work_end_time[new_work]
+        stats.update(
+            frontier_work, new_work, work_start_time[new_work], work_end_time[new_work]
         )
 
     return stats
